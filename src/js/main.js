23e30a925a78aae8e65d9c4b83a1de752d9b27b5
Kathy Dieser <katdiese@gmail.com>
2016-03-22 16:11:11 -0600

    updated typed string

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 $(document).on('ready', function() {
   $('.toolkit').typed({
-    strings: ["Javascript", "Sass", "jQuery", "Node", "Html5", "Express", "Wireframing", "CSS3", "Passport", "Knex", "SQL", "Swift", "Angular", "PostgreSQL", "Pair Programming", "Love."],
+    strings: ["Javascript", "Sass", "jQuery", "Node", "Html5", "Express", "Git and Github version control", "Wireframing", "CSS3", "Passport", "Knex", "SQL", "Swift", "Angular", "PostgreSQL", "Pair Programming", "Love."],
     typeSpeed: 10
   })
 });
