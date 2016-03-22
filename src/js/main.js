c6085e45a38862f7322ff8047f7e1e04780d95da
Kathy Dieser <katdiese@gmail.com>
2016-03-22 13:26:33 -0600

    sticky nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 $(document).on('ready', function() {
   $('.toolkit').typed({
-    strings: ["Javascript", "Sass", "jQuery", "Node", "Html5", "Express", "Wireframing", "CSS3", "Passport", "Knex", "SQL", "Swift", "Angular", "PostgreSQL", "Pair Programming", "Magic"],
+    strings: ["Javascript", "Sass", "jQuery", "Node", "Html5", "Express", "Wireframing", "CSS3", "Passport", "Knex", "SQL", "Swift", "Angular", "PostgreSQL", "Pair Programming", "Love"],
     typeSpeed: 10
   })
 });
