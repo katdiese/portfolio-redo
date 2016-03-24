78d7e5223347dad1c3044bd591c42c20500fd407
Kathy Dieser <katdiese@gmail.com>
2016-03-24 08:35:11 -0600

    function to scroll to id when link clicked

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,8 @@ $(document).on('ready', function() {
   $('.toolkit').typed({
     strings: ["Javascript", "Sass", "jQuery", "Node", "Html5", "Express", "Git and Github version control", "Wireframing", "CSS3", "Passport", "Knex", "SQL", "Swift", "Angular", "PostgreSQL", "Pair Programming", "Love."],
     typeSpeed: 10
-  })
+  });
+
 });
 
 var mainNav = $('.main-nav');
@@ -15,4 +16,19 @@ $(window).scroll(function() {
   } else {
     mainNav.removeClass("main-nav-scrolled");
   }
-})
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+    }
+  });
+});
